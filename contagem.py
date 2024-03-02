c5cad1ec1b2b530d9753e6914e86b504cd0d1d12
Liane Magalhães <liane.mag31@gmail.com>
2024-03-01 21:26:24 -0300

    Create contagem.py

---
+++ contagem.py
@@ -0,0 +1,9 @@
+#Código para contar a quantidade de palavras
+
+lista=['coração', 'paixão', 'amor', 'paixão', 'alegria', 'tristeza', 'amor', 'amor', 'coração', 'alegria', 'tristeza', 'alegria', 'paixão']
+q = lista.count('coração')
+u = lista.count('paixão')
+a = lista.count('amor')
+n = lista.count('alegria')
+t = lista.count('tristeza')
+print('a quantidade de palavras que aparece são: \ncoração:',q, '\npaixão:',u,'\namor:',a, '\nalegria:',n, '\ntristeza:',t,)
